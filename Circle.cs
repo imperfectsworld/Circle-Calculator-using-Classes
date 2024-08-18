a4e255643ccc8dff1eb4b6cf4e9761860a39f5df
imperfectsworld <imperfectsworld@gmail.com>
2024-08-18 16:45:21 -0400

    First commit

---
+++ Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Lab
+{
+    internal class Circle
+    {
+        //properties
+
+        private double Radius { get; set; }
+
+        //constructor
+
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        
+        }
+        //methods
+
+        public double CalculateDiameter()
+        {
+            return 2 * Radius;
+        }
+
+        public double Grow()
+        {
+            return Radius *= 2;
+        }
+
+        public double GetRadius()
+        {
+            return Radius;
+        }
+
+        public double CalculateCircumfrence()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * (Radius * Radius);
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Object_Lab;
+string answer;
+bool repeat = true;
+Circle myCircle;
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+while (true)
+{
+    try
+    {
+        Console.Write("Enter radius: ");
+        myCircle = new Circle(int.Parse(Console.ReadLine()));
+        break;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    
+}
+
+while (repeat == true)
+{
+    Console.WriteLine($"\nDiameter: {myCircle.CalculateDiameter()}");
+    Console.WriteLine($"Circumference: {myCircle.CalculateCircumfrence()}");
+    Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+    Console.Write("Should the circle grow? (y/n): ");
+
+    answer = Console.ReadLine().ToLower();
+
+    if (answer == "y")
+    {
+        myCircle.Grow();
+        Console.WriteLine("The circle is magically growing!");
+    }
+    else
+    {
+        Console.WriteLine($"\nGoodbye. The circle's final radius is {myCircle.GetRadius()}");
+        repeat = false;
+        
+    }
+}
